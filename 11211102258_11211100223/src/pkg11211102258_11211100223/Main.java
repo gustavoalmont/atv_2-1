8da0a416905e80525c70ee207c9ae0bd0a827c81
gustavoalmont <100509300+gustavoalmont@users.noreply.github.com>
2022-03-14 21:43:07 -0300

    Feat Commit
    Atividade de commit

---
+++ 11211102258_11211100223/src/pkg11211102258_11211100223/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg11211102258_11211100223;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Gustavo de Almeida Monteiro");
+        System.out.println("RGM - 11211100223");
+        System.out.println("Idade - 19 anos");
+        System.out.println("Github - @gustavoalmont");
+        System.out.println("Linkedin - https://br.linkedin.com/in/gustavo-de-almeida-monteiro-403939180");
+        System.out.println("Curso - 3º semestre em Sistemas de Informação");
+        
+        System.out.println("Julia Araujo Calazans");
+        System.out.println("RGM - 11211102258");
+        System.out.println("Idade - 18 anos");
+        System.out.println("Github - @juliacalazs");
+        System.out.println("Linkedin - @juliacalazs");
+        System.out.println("Curso - 3º semestre em Sistemas de Informação");
+        
+    }
+    
+}
